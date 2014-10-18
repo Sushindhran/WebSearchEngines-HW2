0ef4966859843e735b510c25daa0b8c9f1a20c32
Harikrishnan <sushindhran@gmail.com>
2014-10-18 18:29:18 -0400

    Added query phrase code.

--- src/edu/nyu/cs/cs2580/QueryPhrase.java
+++ src/edu/nyu/cs/cs2580/QueryPhrase.java
@@ -1,17 +1,47 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
 /**
  * @CS2580: implement this class for HW2 to handle phrase. If the raw query is
  * ["new york city"], the presence of the phrase "new york city" must be
  * recorded here and be used in indexing and ranking.
  */
 public class QueryPhrase extends Query {
+    public Vector< Vector<String>> _phraseTokens = new Vector<Vector<String>>();
 
-  public QueryPhrase(String query) {
-    super(query);
-  }
+    public QueryPhrase(String query) {
+        super(query);
+    }
 
-  @Override
-  public void processQuery() {
-  }
+    @Override
+    public void processQuery() {
+        if (_query == null) {
+            return;
+        }
+        String token;
+        Scanner s1 = new Scanner(_query);
+        Pattern pattern = Pattern.compile("\"[^\"]*\"");
+        while ((token = s1.findInLine(pattern)) != null) {
+            _query = _query.replace(token,"");
+            Vector<String> temp = new Vector<String>();
+            int end = token.length() -1;
+            token = token.substring(1, end);
+            Scanner s2 = new Scanner(token);
+            while (s2.hasNext()) {
+                temp.add(PorterStemming.getStemmedWord(s2.next()));
+            }
+            _phraseTokens.add(temp);
+            s2.close();
+        }
+        s1.close();
+
+        Scanner s3 = new Scanner(_query);
+        while (s3.hasNext()) {
+            _tokens.add(PorterStemming.getStemmedWord(s3.next()));
+        }
+        s3.close();
+    }
 }
